6f4286cd9d4ab35d01941391a0dcb69ea889cc15
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2021-09-05 20:57:55 -0300

    feat: implements theme colors via styled components

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,13 @@
+export default {
+    colors: {
+        primary: '#a5093e',
+
+        success: '#1DB863',
+
+        white: '#fff',
+
+        text: '#666666',
+        icon: '#B2B2B2',
+        background: '#E5E5E5'
+    }
+}
